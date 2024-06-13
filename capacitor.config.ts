54f0e97129f7898b64a8bfd53d8bcab8e7e67d94
Alessio Bongi <alessio.bongi@ekovision.it>
2024-06-13 12:40:01 +0200

    Capacitor SDK Setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
-  webDir: 'www'
+  appId: '79f733f9',
+  appName: 'photo_test',
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '79f733f9',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
